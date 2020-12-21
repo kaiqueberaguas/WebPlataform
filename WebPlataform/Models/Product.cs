b77bb7c31c8960df8a3afbdfbdc47e2fd314d397
Kaique Beraguas <ksakX@outlook.com>
2020-12-21 16:44:33 -0300

    projetando interface e iniciando backend

--- WebPlataform/Models/Product.cs
+++ WebPlataform/Models/Product.cs
@@ -4,6 +4,6 @@
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string image { get; set; }
+        public string Image { get; set; }
     }
 }
